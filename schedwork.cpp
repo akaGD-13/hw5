c8d6802b61460b712367c932d796112cc22a9d7c
Guangda Fei <guangdaf@usc.edu>
2023-04-07 07:32:19 +0000

    all done with hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,11 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool schedule_helper(int day, int position, AvailabilityMatrix avail,
+    const size_t dailyNeed,
+    const size_t maxShifts, vector<int>& maxShifts_vec,
+    DailySchedule& sched
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,8 +41,63 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    vector<int> maxShifts_vec;
+    for (size_t i=0; i < avail[0].size(); i++){
+        maxShifts_vec.push_back(0);
+    }
+
+    for (size_t i=0; i < avail.size(); i++){
+        vector<Worker_T> temp;
+        for (int i=0; i<dailyNeed; i++){
+          temp.push_back(-1);
+        }
+        sched.push_back(temp);
+    }
+    
+
+    return schedule_helper(0,0,avail, dailyNeed, maxShifts, maxShifts_vec, sched);
+
+}
 
 
+bool schedule_helper(int day, int position,  AvailabilityMatrix avail,
+    const size_t dailyNeed,
+    const size_t maxShifts, vector<int>& maxShifts_vec,
+    DailySchedule& sched
+)
+{
+    //base case
+    if (day >= sched.size()){
+        return true;
+    }
+
+    for (size_t i=0; i < avail[day].size(); i++){ // i is the Worker_ID
+        if (avail[day][i] == 1){// avaliable
+            sched[day][position] = i;
+            maxShifts_vec[i]+= 1;
+            avail[day][i] = 0;
+
+            if (maxShifts_vec[i] <= maxShifts){ 
+                // next recursion
+                bool status;
+                if (position == dailyNeed -1){//last postion of the day
+                    status = schedule_helper(day+1,0,avail, dailyNeed, maxShifts, maxShifts_vec, sched); //new day, first position
+                }
+                else{
+                    status = schedule_helper(day,position+1,avail, dailyNeed, maxShifts, maxShifts_vec, sched);
+                }
+
+                if (status == true){
+                    return true;
+                }
+            }
+            maxShifts_vec[i] -= 1; // this option does not work, reset it 
+            avail[day][i] = 1;         
+        }
+        
+    }
+
+    return false; // run out of all options
 
 
 }
--- wordle.cpp
+++ wordle.cpp
@@ -9,10 +9,19 @@
 
 #include "wordle.h"
 #include "dict-eng.h"
+#include <string>
 using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordle_helper(std::string in,
+    std::string floating,
+    std::set<std::string>& solutions, const std::set<std::string>& dict, int num_empty);
+
+//check if the string contains the floating
+// bool check(std::string s, const std::string& in, const std::string& floating);
+
+int numofchar(string s, char c);
 
 
 // Definition of primary wordle function
@@ -22,7 +31,78 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<std::string> solutions;
+    int num_empty = numofchar(in, '-');
+    wordle_helper(in, floating, solutions, dict, num_empty);
+
+    return solutions;
+    
+}
+
+
+
+
+// Define any helper functions here
+void wordle_helper(std::string in,
+    std::string floating,
+    std::set<std::string>& solutions, const std::set<std::string>& dict, int num_empty)
+{
+  //base case
+  if (num_empty == 0){
+    if (dict.find(in) != dict.end()){
+      solutions.insert(in);
+      return;
+    }
+  }
+
+  size_t index = in.find('-'); // index of the first '-'
+  string chars;
+  if (floating.size() == num_empty){
+    chars = floating;
+  }
+  else{
+    chars = "abcdefghijklmnopqrstuvwxyz";
+  }
+  for (auto x : chars){
+      in[index] = x;
+      string new_f = floating;
+      if (floating.find(x) != string::npos){
+        new_f.erase(new_f.begin()+new_f.find(x));
+      }
+      wordle_helper(in, new_f, solutions, dict, num_empty-1);
+  }
 
 }
 
-// Define any helper functions here
+// bool check(std::string s, const std::string& in, const std::string& floating){
+
+//     // replace original letters with '-'
+//     for (size_t i=0; i< s.size(); i++){
+//       if (in[i] != '-'){
+//         s[i] = '-';
+//       }
+//     }
+
+//     for (size_t i=0; i < floating.size(); i++){
+//       if (s.find(floating[i]) == string::npos){
+//         // floating doesn't exist
+//         return false;
+//       }
+//       else{
+//         //found;
+//         s[s.find(floating[i])] = '-';
+//       }      
+//     }
+
+//     return true;
+// }
+
+int numofchar(string s, char c){
+  int count = 0;
+  for (size_t i=0; i < s.size(); i++){
+    if (s[i] == c){
+      count++;
+    }
+  }
+  return count;
+}
